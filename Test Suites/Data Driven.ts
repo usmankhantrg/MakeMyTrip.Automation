e75f1086d599c84925fe66c85ef06ce886eadeaf
usmankhantrg <usmankhantrg@gmail.com>
2024-07-10 05:06:43 +0100

    Test Suite

--- Test Suites/Data Driven.ts
+++ Test Suites/Data Driven.ts
@@ -16,17 +16,26 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Data Driven/Login_DataDriven</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ac0663a6-c9dd-4d25-9bcb-518f1ae080a8</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Logins (1)</testDataId>
+      </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>ac0663a6-c9dd-4d25-9bcb-518f1ae080a8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>UserName</value>
          <variableId>b8a22e65-ee5d-45b3-8989-e7e70338904f</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
+         <testDataLinkId>ac0663a6-c9dd-4d25-9bcb-518f1ae080a8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
          <variableId>338054be-7a86-40ca-8807-750658253e8b</variableId>
       </variableLink>
    </testCaseLink>
