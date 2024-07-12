ea0992c200e5baf525a785cba2c0798b4218cb0c
usmankhantrg <usmankhantrg@gmail.com>
2024-07-12 19:09:31 +0100

    Test Case Correction

---
+++ Object Repository/CURAHealthcareService/Page_CURA Healthcare Service/MakeAppointment_button.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MakeAppointment_button</name>
+   <tag></tag>
+   <elementGuidId>6b796474-92b6-47e0-a1f5-8f0d4d8214bf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;btn-make-appointment&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
