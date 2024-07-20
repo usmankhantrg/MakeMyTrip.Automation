28cfbb71ceb9aa1934ac29287ac7c7b54d01fa5f
usmankhantrg <usmankhantrg@gmail.com>
2024-07-20 15:05:00 +0100

    XYZ Bank

---
+++ Object Repository/XYZBank/StartPage/LoginAsCustomer.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>LoginAsCustomer</name>
+   <tag></tag>
+   <elementGuidId>f9aee374-51f1-4559-a14b-72c1541966a3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div/div/div[2]/div/div[1]/div[1]/button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
