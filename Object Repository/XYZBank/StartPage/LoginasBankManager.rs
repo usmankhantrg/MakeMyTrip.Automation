49eca94528abf7c3f2f600c0fc37c4189ce2fd26
usmankhantrg <usmankhantrg@gmail.com>
2024-07-20 15:05:19 +0100

    XYZBank test cases

---
+++ Object Repository/XYZBank/StartPage/LoginasBankManager.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>LoginasBankManager</name>
+   <tag></tag>
+   <elementGuidId>4c9add70-beee-48b2-ac6c-82781d4a766d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div/div/div[2]/div/div[1]/div[2]/button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
